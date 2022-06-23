ae486a5b67a1869d7f8da808357999eab05c0442
sonydotgit <forhoooman@gmail.com>
2022-06-23 23:38:01 +0530

    Display finished count

--- domp3.sh
+++ domp3.sh
@@ -8,11 +8,12 @@ do
 
     yt-dlp -f 'ba' -x --audio-format mp3 "$olnk" -o 'down/%(title)s.%(ext)s'
 
-    echo "****************************************"
-    echo $olnk $ofr $oto
-    echo "****************************************"
-
     count=$((count+1))
+
+    echo "****************************************"
+    echo $olnk
+    echo $count done
+    echo "****************************************"
 
 done < links.txt
 
